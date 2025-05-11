afe786e25c18e699a077e05b98e292f07226d339
Karolina Viragova <karolinavi94@gmail.com>
2025-05-11 13:05:31 +0200

    solution

--- src/components/TShirtSetup/index.jsx
+++ src/components/TShirtSetup/index.jsx
@@ -1,10 +1,23 @@
 import './style.css';
 
-export const TShirtSetup = () => {
+export const TShirtSetup = ({onTypeChange, onColorChange, onPrintChange}) => {
+
+  const chnagesToSeletion = (e) => {
+    onTypeChange(e.target.value)
+  }
+
+  const changesToColor = (e) => {
+    onColorChange(e.target.value)
+  }
+
+  const changesToPrint = (e) => {
+    onPrintChange(e.target.value)
+  }
+
   return (
     <form className="tshirt-setup">
       <label className="tshirt-setup__label">Typ:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={chnagesToSeletion}>
         <option value="normalShortSleeve">Krátký rukáv</option>
         <option value="tallShortSleeve">Krátký rukáv (prodloužené)</option>
         <option value="normalLongSleeve">Dlouhý rukáv</option>
@@ -14,7 +27,7 @@ export const TShirtSetup = () => {
       </select>
 
       <label className="tshirt-setup__label">Barva:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={changesToColor}>
         <option value="yellow">Žlutá</option>
         <option value="red">Červená</option>
         <option value="blue">Modrá</option>
@@ -22,7 +35,7 @@ export const TShirtSetup = () => {
       </select>
     
       <label className="tshirt-setup__label">Potisk:</label>
-      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" />
+      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" onChange={changesToPrint}/>
     </form>
   );
 };
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import { TShirt } from '../../components/TShirt';
 import { TShirtSetup } from '../../components/TShirtSetup';
 import './style.css';
 
 export const HomePage = () => {
+
+  const [typeOfTshirt, setTypeOfTshirt] = useState("normalShortSleeve" );
+  const [colorOfTshirt, setColorOfTshirt] = useState("yellow");
+  const [textOnTshirt, setTextOnTshirt] = useState("Ahoj světe");
+
+  const changeOftype = (type) => {
+    setTypeOfTshirt(type)
+  }
+
+  const changeOfColor = (color) => {
+    setColorOfTshirt(color)
+  }
+
+  const changeOfText = (text) => {
+    setTextOnTshirt(text)
+  }
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt type={typeOfTshirt} color={colorOfTshirt} print={textOnTshirt}/>
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup onTypeChange={changeOftype} onColorChange={changeOfColor} onPrintChange={changeOfText}/>
         </div>
       </div>
     </div>
